16583c82db6c0c2ffc5db21f0954b7f63e215d43
millie-cx3 <millielc2024@gmail.com>
2025-10-01 19:18:15 +0000

    finished sections 1&2

---
+++ tasks/task2_5_1/valtest.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var number = 20
+    println(number)
+    number = 15
+    println(number)
+}
+
+// valtest.kt:4:5: error: 'val' cannot be reassigned.
+//    number = 15
+//    ^^^^^^
---
+++ tasks/task2_5_2/variables.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val myAge = 29u
+    val univeriseAge = 13_800_000_000L
+    val status = 'M'
+    val name = "Sarah"
+    val height = 1.78f
+    val root2 = Math.sqrt(2.0)
+
+    print(myAge::class)
+    print(univeriseAge::class)
+    print(status::class)
+    print(name::class)
+    print(height::class)
+    print(root2::class)
+}
+
+// UInt
+// Long
+// Char
+// String
+// Float
+// Double
